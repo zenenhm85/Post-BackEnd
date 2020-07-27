836ad060e7f2a5889c61b49456079f2b5e89a88a
Zenén Hernández Martínez <zenenhernandez85@gmail.com>
2020-07-27 01:09:38 +0100

    configuring server

---
+++ config.js
@@ -0,0 +1,8 @@
+/*=============================================
+El process es un objeto global que corre en todo el entorno de desarrollo de nodeJS
+=============================================*/
+process.env.PORT = process.env.PORT || 4000;
+
+process.env.SECRET = "noquieroquesesepa";
+
+process.env.CADUCIDAD = 60*60*24*30;
---
+++ index.js
@@ -0,0 +1,71 @@
+/*=============================================
+REQUERIMIENTOS
+=============================================*/
+require('./config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+/*=============================================
+VARIABLE PARA TENER TODAS LAS FUNCIONALIDADES DE EXPRESS
+=============================================*/
+
+const app = express();
+
+/*=============================================
+MIDDLEWARE PARA BODY PARSER
+=============================================*/
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true }));
+
+// parse application/json
+app.use(bodyParser.json({limit: '10mb', extended: true}));
+
+/*=============================================
+MIDDLEWARE PARA FILEUPLOAD
+=============================================*/
+
+// default options express-fileupload
+app.use(fileUpload());
+
+
+/*=============================================
+MONGOOSE DEPRECATIONS
+=============================================*/
+// https://mongoosejs.com/docs/deprecations.html
+const optionsMongoose={
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+}
+
+/*=============================================
+RUTAS
+=============================================*/
+
+
+
+/*=============================================
+CONEXIÓN A LA BASE DE DATOS
+=============================================*/
+
+mongoose.connect('mongodb://localhost:27017/apirest-post', optionsMongoose, (err, res)=>{
+
+	if(err) throw err;
+
+	console.log("Conected to Data Base")
+
+});
+
+/*=============================================
+SALIDA PUERTO HTTP
+=============================================*/
+app.listen(process.env.PORT, ()=>{
+
+	console.log(`Enabled to port: ${process.env.PORT}`)
+})
+
