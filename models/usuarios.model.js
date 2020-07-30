d7d651e4a8daae8c510001aa9517c854a2b5635b
Zenén Hernández Martínez <zenenhernandez85@gmail.com>
2020-07-30 14:28:38 +0100

    server-side validations

--- models/usuarios.model.js
+++ models/usuarios.model.js
@@ -3,6 +3,9 @@ ESQUEMA PARA EL MODELO CONECTOR A MONGODB
 =============================================*/
 const mongoose = require('mongoose');
 
+//Requerimos el módulo para validaciones únicas
+const uniqueValidator = require('mongoose-unique-validator');
+
 let Schema = mongoose.Schema;
 
 let usuariosSchema = new Schema({
@@ -37,6 +40,13 @@ usuariosSchema.methods.toJSON = function(){
 	return usuarioObject;
 
 }
+
+/*=============================================
+Devolver mensaje personalizado para validaciones únicas
+=============================================*/
+
+usuariosSchema.plugin(uniqueValidator, {message: 'El {PATH} ya está registrado en la Base de datos' })
+
 
 /*=============================================
 EXPORTAMOS EL MODELO
